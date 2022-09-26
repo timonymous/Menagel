f3cf5d5c9eb3a93863c51b5f5435f99064acfab3
Kevin - niveau systeme <c2e31>
2022-09-26 16:27:48 +0200

    methode fini

--- Menagel/manager/CommandLineManager.cs
+++ Menagel/manager/CommandLineManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,73 @@ namespace Menagel.manager
 {
     class CommandLineManager
     {
+
+        public static MySqlConnection Connexion()
+        {
+            MySqlConnection connexion = new MySqlConnection("server = localhost;" +
+                "database=menageleccsharp;" +
+                "port=3306;" +
+                "user=toto;" +
+                "password=toto;");
+
+
+            return connexion;
+        }
+
+        //readById
+        public List<entity.LigneCommande> readById()
+        {
+            List<entity.LigneCommande> LcCollecte = new List<entity.LigneCommande>();
+            MySqlConnection connect = new MySqlConnection();
+            connect.Close();
+            connect = Connexion();
+            connect.Open();
+            MySqlCommand cmd = connect.CreateCommand();
+
+            cmd.CommandText = "select * from ligneCommande WHERE commande = @Commande";
+
+            MySqlDataReader collecte = cmd.ExecuteReader();
+            while (collecte.Read())
+            {
+                entity.LigneCommande coll = new entity.LigneCommande();
+
+                coll.Commande = collecte.GetInt32("commande");
+
+
+                LcCollecte.Add(coll);
+            }
+            connect.Close();
+            return LcCollecte;
+        }
+
+        //readAll
+        public List<entity.LigneCommande> findAll()
+        {
+            List<entity.LigneCommande> LcCollecte = new List<entity.LigneCommande>();
+            MySqlConnection connect = new MySqlConnection();
+            connect.Close();
+            connect = Connexion();
+            connect.Open();
+            MySqlCommand cmd = connect.CreateCommand();
+
+            cmd.CommandText = "select * from ligneCommande";
+
+            MySqlDataReader collecte = cmd.ExecuteReader();
+            while (collecte.Read())
+            {
+                entity.LigneCommande coll = new entity.LigneCommande();
+
+                coll.Produit = collecte.GetInt32("produit");
+                coll.Commande = collecte.GetInt32("commande");
+                coll.Quanity = collecte.GetInt32("quantite");
+                
+
+                LcCollecte.Add(coll);
+            }
+            connect.Close();
+            return LcCollecte;
+        }
+
     }
+}
 }
